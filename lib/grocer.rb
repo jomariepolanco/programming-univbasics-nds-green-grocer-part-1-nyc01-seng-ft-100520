bab834d049d22f49b029dcdf8bb5239a76d9fbfa
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-24 14:59:52 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -27,7 +27,7 @@ def consolidate_cart(cart)
         new_cart_item[:count] += 1 
       else 
         new_cart_item = {
-          item => cart[i][:item]
+          :item => cart[i][:item]
         }
       end 
     i += 1 
